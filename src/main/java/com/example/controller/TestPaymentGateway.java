e5fcf1de39dca4e7e8d4e301450c640a3f55cb6a
Panda <sachinpanda98@gmail.com>
2023-12-28 11:16:52 +0530

    4521: was unable to see payment transaction details in stripe.

--- src/main/java/com/example/controller/TestPaymentGateway.java
+++ src/main/java/com/example/controller/TestPaymentGateway.java
@@ -2,10 +2,10 @@ package com.example.controller;
 
 public class TestPaymentGateway {
     public static void main(String[] args) {
-        // Create an instance of TestPaymentGateway
+
         TestPaymentGateway testPaymentGateway = new TestPaymentGateway();
 
-        // Perform actions with testPaymentGateway
+
         System.out.println("Using TestPaymentGateway in SomeOtherClass");
     }
 }
