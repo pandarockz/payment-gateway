053625271ccc3b0c091efee7f33a181121ba2a7f
Panda <sachinpanda98@gmail.com>
2023-12-28 11:42:12 +0530

    4521: was unable to see payment transaction details in stripe.

--- src/main/java/com/example/controller/TestPaymentGateway.java
+++ src/main/java/com/example/controller/TestPaymentGateway.java
@@ -1,11 +1,5 @@
 package com.example.controller;
 
 public class TestPaymentGateway {
-    public static void main(String[] args) {
 
-     
-            TestPaymentGateway.someStaticMethod();
-    }
-    private static void someStaticMethod() {
-    }
 }
